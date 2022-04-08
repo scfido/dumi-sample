a9f30ddb9eea3c2d7f38b97dfc89794d5746192f
fuyun <scfido@gmail.com>
2022-04-08 14:13:30 +0800

    添加less样式

--- src/Foo/index.tsx
+++ src/Foo/index.tsx
@@ -1,3 +1,6 @@
 import React from 'react';
+import styles from 'style.less';
 
-export default ({ title }: { title: string }) => <h1>{title}</h1>;
+export default ({ title }: { title: string }) => (
+  <h1 className={styles.header}>{title}</h1>
+);
